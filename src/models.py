f1cf2d44bc45ceb44acf5926f95be5d2b30fcc52
adriancastro626 <adriancastro626@icloud.com>
2021-04-01 04:39:49 +0000

    resolved

--- src/models.py
+++ src/models.py
@@ -8,23 +8,43 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), primary_key=True)
+    user_name = Column(String(250), primary_key=True)
+    password = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+
+class Fav_Planet(Base):
+    __tablename__ = 'fav_planet'
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    planet_id = Column(Integer, primary_key=True)  
+    person = relationship(User)
+
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, ForeignKey('fav_planet.id'), primary_key=True)
+    name = Column(String, nullable=False)
+    population = Column(Integer, nullable=False)
+    terrain = Column(String, nullable=False)
+    person = relationship(Fav_Planet)
+
+class Fav_People(Base):
+    __tablename__ = 'fav_people'
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    people_id = Column(Integer, primary_key=True)  
+    person = relationship(User)
+
+class People(Base):
+    __tablename__ = 'people'
+    id = Column(Integer, ForeignKey('fav_people.id'), primary_key=True)
+    name = Column(String, nullable=False)
+    hair_color = Column(String, nullable=False)
+    eyes_color = Column(String, nullable=False)
+    person = relationship(Fav_People)
 
     def to_dict(self):
         return {}
